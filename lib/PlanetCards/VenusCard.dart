aa838d8c9a5c3f93961bef5879240098a8de0c8e
arora <aroramadhav65@gmail.com>
2022-04-16 20:45:53 +0530

    Update

--- lib/PlanetCards/VenusCard.dart
+++ lib/PlanetCards/VenusCard.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: file_names
 
 import 'package:flutter/material.dart';
+import 'package:solar_system_app/PlanetInfos/VenusInfo.dart';
 
 class VenusCard extends StatelessWidget {
   @override
