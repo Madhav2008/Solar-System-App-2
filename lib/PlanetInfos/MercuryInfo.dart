8247987455d3c391170f352364c286268784ade9
arora <aroramadhav65@gmail.com>
2022-04-16 20:52:55 +0530

    Update

--- lib/PlanetInfos/MercuryInfo.dart
+++ lib/PlanetInfos/MercuryInfo.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: file_names, use_key_in_widget_constructors, prefer_const_constructors, sized_box_for_whitespace, avoid_unnecessary_containers
+// ignore_for_file: file_names, use_key_in_widget_constructors, prefer_const_constructors, sized_box_for_whitespace, avoid_unnecessary_containers, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 
