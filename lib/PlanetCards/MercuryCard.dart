8bd28f8d4820143e48ec17428bc3d53dcdd27719
arora <aroramadhav65@gmail.com>
2022-04-16 20:22:58 +0530

    Update

--- lib/PlanetCards/MercuryCard.dart
+++ lib/PlanetCards/MercuryCard.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: file_names
 
 import 'package:flutter/material.dart';
-import 'package:solar_system_app/screens/MercuryInfo.dart';
 
 class MercuryCard extends StatelessWidget {
   @override
