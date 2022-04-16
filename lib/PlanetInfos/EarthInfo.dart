eff681847ef2a92f3eb9e3dcc00c38f7966fabbf
arora <aroramadhav65@gmail.com>
2022-04-16 20:42:27 +0530

    Update

--- lib/PlanetInfos/EarthInfo.dart
+++ lib/PlanetInfos/EarthInfo.dart
@@ -1,127 +1,149 @@
 // ignore_for_file: file_names
 
 import 'package:flutter/material.dart';
-import 'package:solar_system_app/PlanetInfos/MercuryInfo.dart';
 
-class EarthCard extends StatelessWidget {
+class MercuryInfo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () => Navigator.push(
-        context,
-        MaterialPageRoute(
-          builder: (_) => MercuryInfo(),
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('../assets/background.jpg'),
+            fit: BoxFit.cover,
+          ),
         ),
-      ),
-      child: Container(
-        margin: EdgeInsets.symmetric(horizontal: 45),
-        height: 450,
-        child: Stack(
+        child: Column(
           children: <Widget>[
-            Center(
-              child: Card(
-                margin: EdgeInsets.only(top: 60),
-                clipBehavior: Clip.antiAliasWithSaveLayer,
-                elevation: 7,
-                color: Color.fromARGB(255, 2, 44, 78),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30),
-                ),
-                child: Container(
-                  width: 290,
-                  color: Color.fromARGB(255, 2, 44, 78),
+            Align(
+              alignment: Alignment.topLeft,
+              child: Padding(
+                padding: EdgeInsets.only(top: 40),
+                child: FlatButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: Icon(
+                    Icons.close,
+                    color: Colors.white,
+                    size: 30,
+                  ),
                 ),
               ),
             ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
+            Stack(
               children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.only(left: 55),
-                  child: Center(
+                Align(
+                  alignment: Alignment.center,
+                  child: Container(
+                    height: 600,
+                    width: double.infinity,
+                    color: Colors.transparent,
+                  ),
+                ),
+                Center(
+                  child: Card(
+                    margin: EdgeInsets.only(top: 100),
+                    clipBehavior: Clip.antiAliasWithSaveLayer,
+                    elevation: 7,
+                    color: Color.fromARGB(255, 2, 44, 78),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
                     child: Container(
-                      height: 170,
-                      width: 180,
-                      child: ClipRRect(
-                        child: Image.asset('../assets/Mercury.png'),
-                      ),
+                      width: 290,
+                      height: 525,
+                      color: Color.fromARGB(255, 2, 44, 78),
                     ),
                   ),
                 ),
-                Container(
-                  padding: EdgeInsets.only(left: 45),
-                  child: Column(
-                    children: <Widget>[
-                      Text(
-                        'Mercury',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 40,
-                          fontWeight: FontWeight.w500,
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Center(
+                      child: Container(
+                        height: 180,
+                        width: 200,
+                        child: ClipRRect(
+                          child: Image.asset('../assets/Mercury.png'),
                         ),
                       ),
-                      Text(
-                        'Planet',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                          fontWeight: FontWeight.w300,
-                        ),
+                    ),
+                    Container(
+                      child: Column(
+                        children: <Widget>[
+                          Text(
+                            'Mercury',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 40,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          Text(
+                            'Planet',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 15,
+                              fontWeight: FontWeight.w300,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 25,
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(left: 65),
+                            child: Row(
+                              children: <Widget>[
+                                Text(
+                                  'Distance from sun: \nRadius: \nGravity: \nOrbital Period: \nLength of a day: \nMass: ',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.w500,
+                                    height: 1.5,
+                                  ),
+                                ),
+                                Text(
+                                  '57.91 million km \n2,439.7 km \n3.7 m/s² \n88 days \n58d 15h 30m \n3.285 x 10^23 kg',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.w200,
+                                    height: 1.5,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Text(
+                            'OVERVIEW',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.w500,
+                              height: 2,
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 65),
+                            child: Text(
+                              'Mercury is the smallest and innermost planet in the Solar System. Its orbit around the Sun takes only 87.97 days, the shortest of all the planets in the Solar System. It is named after the Roman deity Mercury, the messenger of the gods.',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w200,
+                                height: 1.5,
+                              ),
+                            ),
+                          )
+                        ],
                       ),
-                      SizedBox(height: 25),
-                      Text(
-                        'Distance from sun',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                      Text(
-                        '57.51 million km',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                          fontWeight: FontWeight.w200,
-                        ),
-                      ),
-                      SizedBox(height: 20),
-                      Text(
-                        'Radius',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                      Text(
-                        '2,339.7 km',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                          fontWeight: FontWeight.w200,
-                        ),
-                      ),
-                      SizedBox(height: 20),
-                      Text(
-                        'Gravity',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                      Text(
-                        '3.7 m/s²',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                          fontWeight: FontWeight.w200,
-                        ),
-                      ),
-                    ],
-                  ),
+                    )
+                  ],
                 )
               ],
             )
