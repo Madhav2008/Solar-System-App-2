44b95ad5c96c6c222f44201f373ce60d55fedef5
arora <aroramadhav65@gmail.com>
2022-04-16 20:51:44 +0530

    Update

--- lib/PlanetInfos/MarsInfo.dart
+++ lib/PlanetInfos/MarsInfo.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: file_names, use_key_in_widget_constructors, prefer_const_constructors
+// ignore_for_file: file_names, use_key_in_widget_constructors, prefer_const_constructors, sized_box_for_whitespace
 
 import 'package:flutter/material.dart';
 
