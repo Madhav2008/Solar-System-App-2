ecbc3563cfcd38f5400efb14eeff5a20070eff64
arora <aroramadhav65@gmail.com>
2022-04-16 20:21:36 +0530

    Update

--- lib/Screens/Home.dart
+++ lib/Screens/Home.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: file_names
+
 import 'package:flutter/material.dart';
 import 'package:solar_system_app/PlanetCards/EarthCard.dart';
 import 'package:solar_system_app/PlanetCards/MarsCard.dart';
