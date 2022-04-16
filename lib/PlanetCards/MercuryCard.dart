5bfdf381f7b0c53988570360cca607a9170f2191
arora <aroramadhav65@gmail.com>
2022-04-16 19:56:58 +0530

    Update

--- lib/PlanetCards/MercuryCard.dart
+++ lib/PlanetCards/MercuryCard.dart
@@ -6,7 +6,7 @@ class MercuryCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.symmetric(horizontal: 45),
-      height: 450,
+      height: 460,
       child: Stack(
         children: <Widget>[
           Center(
