2dd9e9cc5f061553056f6c7b8524684295625d20
arora <aroramadhav65@gmail.com>
2022-04-16 20:12:50 +0530

    Update

--- lib/PlanetCards/MercuryCard.dart
+++ lib/PlanetCards/MercuryCard.dart
@@ -39,7 +39,7 @@ class MercuryCard extends StatelessWidget {
                 padding: EdgeInsets.only(left: 55),
                 child: Center(
                   child: Container(
-                    height: 180,
+                    height: 160,
                     width: 180,
                     child: ClipRRect(
                       child: Image.asset('../assets/Mercury.png'),
